1deebdbb54a3416286112044dccde86ccac92d46
Javipopotter <javivals@ucm.es>
2025-02-17 18:20:43 +0100

    Update Transform.h

--- TPV2/src/components/Transform.h
+++ TPV2/src/components/Transform.h
@@ -5,6 +5,9 @@
 class Transform : public Component
 {
 public:
+	Transform(float x, float y) : _position(Vector2D(x,y)), _velocity(Vector2D(0,0)) {}
+	~Transform() {};
+
 	void Set(float x, float y);
 	void Translate(float x, float y);
 	void SetVelocity(float x, float y);
