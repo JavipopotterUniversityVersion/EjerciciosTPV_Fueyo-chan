9e5cf7126830097838aac07ced9fe44070939ae5
Javipopotter <javivals@ucm.es>
2025-02-24 18:48:59 +0100

    Image_With_Frames.h

--- TPV2/src/components/Image_With_Frames.cpp
+++ TPV2/src/components/Image_With_Frames.cpp
@@ -1,11 +1,20 @@
 #include "Image_With_Frames.h"
 #include "../ecs/Entity.h"
 #include "../ecs/Manager.h"
+#include "../sdlutils/SDLUtils.h"
 
 void Image_With_Frames::initComponent() {
 	_img = _ent->getMngr()->getComponent<Image>(_ent);
 }
 
 void Image_With_Frames::update() {
-	//_img->setTexture();
+	timer += sdlutils().deltaTime();
+
+	if (timer >= 0.5f)
+	{
+		Texture* tex = _textures.front();
+		_textures.pop();
+		_textures.push(tex);
+		_img->setTexture(tex);
+	}
 }
