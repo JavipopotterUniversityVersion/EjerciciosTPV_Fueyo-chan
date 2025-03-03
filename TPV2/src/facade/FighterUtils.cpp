13ea86ab1d1db72cf2db75624789b6084c9f8601
Javipopotter <javivals@ucm.es>
2025-03-03 18:12:57 +0100

    estoy ahi con el faiterutils

--- TPV2/src/facade/FighterUtils.cpp
+++ TPV2/src/facade/FighterUtils.cpp
@@ -2,25 +2,25 @@
 #include "FighterUtils.h"
 #include "ecs/Manager.h"
 #include "../components/Image.h"
+#include "../components/Health.h"
 
 FighterUtils::FighterUtils(ecs::Manager* manager) : _fighter(nullptr), _manager(manager) {}
 
 void FighterUtils::create_fighter()
 {
-	ecs::Manager* mng = new ecs::Manager();
-	_fighter = mng->addEntity(ecs::grp::PLAYER);
-	mng->addComponent<Transform>();
-	mng->addComponent<Image>();
+	_fighter = _manager->addEntity(ecs::grp::PLAYER);
+	_manager->addComponent<Transform>();
+	_manager->addComponent<Image>();
 }
 
 void FighterUtils::reset_fighter()
 {
-
+	_manager->getComponent<
 }
 
 void FighterUtils::reset_lives()
 {
-
+	_manager->getComponent<Health>(_fighter)->ResetHealth();
 }
 
 int FighterUtils::update_lives(int n)
