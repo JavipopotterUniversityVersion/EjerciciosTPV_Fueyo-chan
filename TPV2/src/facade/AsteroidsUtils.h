0778797b0822bec08eb8d57750442dcf89db996f
Javipopotter <javivals@ucm.es>
2025-03-03 18:39:04 +0100

    Estoy cansado

--- TPV2/src/facade/AsteroidsUtils.h
+++ TPV2/src/facade/AsteroidsUtils.h
@@ -3,5 +3,10 @@
 
 class AsteroidsUtils : public AsteroidsFacade
 {
-
+public:
+	AsteroidsUtils() {}
+	~AsteroidsUtils() {}
+	virtual void create_asteroids(int n) override;
+	virtual void remove_all_asteroids() override;
+	virtual void split_astroid(ecs::Entity* a) override;
 };
