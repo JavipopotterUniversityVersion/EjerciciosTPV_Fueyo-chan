febc883d5de99e105c158f0701f1113b63990d01
Javipopotter <javivals@ucm.es>
2025-03-06 16:22:20 +0100

    z

--- TPV2/src/components/Health.cpp
+++ TPV2/src/components/Health.cpp
@@ -4,6 +4,13 @@
 Health::~Health() {}
 
 void
-Health::ShowHealth() {
+Health::render() {
+	int width = sdlutils().width();
+	int height = sdlutils().height();
 
+	for (int c = 0; c < _health; c++)
+	{
+		SDL_Rect rect{ width / 5 * (c + 1), height / 10, HEART_SIZE, HEART_SIZE };
+		_tex->render(rect);
+	}
 }
--- TPV2/src/components/Health.h
+++ TPV2/src/components/Health.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "../ecs/Component.h"
+#include "../sdlutils/Texture.h"
+#include "../sdlutils/SDLUtils.h"
 
 class Health : public ecs::Component
 {
 public:
-	Health(int h) : _health(h) {};
+	Health(int h) : _health(h), _tex(&sdlutils().images().at("heart")) {};
 	~Health();
 
-	void ShowHealth();
+	void render() override;
 	void LoseHealth(int h) { _health -= h; }
 	void ResetHealth() { _health = 3; }
 	const int GetHealth() { return _health; }
@@ -15,5 +17,7 @@ public:
 	__CMPID_DECL__(ecs::cmp::HEALTH)
 
 private:
+	static constexpr int HEART_SIZE = 32;
 	int _health = 3;
+	Texture* _tex;
 };
