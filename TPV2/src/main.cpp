805d17b54719fbf10c8b27abab455d528f6b9d65
JaviFueyo <jfueyo@ucm.es>
2025-04-07 18:57:29 +0200

    Update main.cpp

--- TPV2/src/main.cpp
+++ TPV2/src/main.cpp
@@ -8,7 +8,8 @@ int main(int, char**) {
 
 	try {
 		Game g;
-		g.init();
+		g.Init();
+		g.initGame();
 		g.start();
 	} catch (const std::string &e) { // catch exceptions thrown as strings
 		std::cerr << e << std::endl;
