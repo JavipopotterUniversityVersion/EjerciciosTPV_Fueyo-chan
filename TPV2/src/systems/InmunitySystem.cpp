8997601721d099f474db78b89356a3f70d24daa9
Javipopotter <153302487+JavipopotterUniversityVersion@users.noreply.github.com>
2025-05-17 13:08:56 +0200

    Update InmunitySystem.cpp

--- TPV2/src/systems/InmunitySystem.cpp
+++ TPV2/src/systems/InmunitySystem.cpp
@@ -25,10 +25,13 @@ void InmunitySystem::recieve(const Message& m) {
 		isInmune = true;
 		_nextTime = sdlutils().virtualTimer().currTime() + INMUNE_TIME;
 		break;
-	case _m_WONDER_FRUIT_EATEN:
-		Message m;
-		m.id = _m_IMMUNITY_START;
-		_mngr->send(m);
+	case _m_PACMAN_FOOD_COLLISION:
+		if (m.pacman_food_collision_data.is_wonder_and_active)
+		{
+			Message m;
+			m.id = _m_IMMUNITY_START;
+			_mngr->send(m);
+		}
 		break;
 	default:
 		break;
